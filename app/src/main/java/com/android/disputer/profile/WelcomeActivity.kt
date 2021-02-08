41b300467c862218eccf2908032f0cf7b2ebffeb
Dominic Flocea <dominicflocea.dev@gmail.com>
2021-02-08 20:55:32 +0200

    Added more editable data

--- app/src/main/java/com/android/disputer/profile/WelcomeActivity.kt
+++ app/src/main/java/com/android/disputer/profile/WelcomeActivity.kt
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.android.disputer.R
+import com.android.disputer.schema.User
 
 class WelcomeActivity : AppCompatActivity() {
-    private val username: String = "Vdovicenco"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        val helloText: TextView = findViewById<TextView>(R.id.helloText)
-        helloText.text = getString(R.string.hello_text, username)
+        val user = User()
+
+        val name: TextView = findViewById<TextView>(R.id.name)
+        val username: TextView = findViewById<TextView>(R.id.username)
+        name.text = getString(R.string.user, user.firstName, user.lastName)
+        username.text = getString(R.string.username, user.username)
     }
 }
