08f58dd4d57ba69ede792915c193234140e47aa9
Dominic Flocea <dominicflocea.dev@gmail.com>
2021-02-08 16:42:54 +0200

    Added user schema and enum for avatars

---
+++ app/src/main/java/com/android/disputer/schema/User.kt
@@ -0,0 +1,18 @@
+package com.android.disputer.schema
+
+import com.android.disputer.enums.Avatar
+
+class User {
+    private var username: String = "Vdovicenco"
+    private var email: String = "bossfaf.vdovicenco@gmail.com"
+    private var avatar: Avatar = Avatar.GIVE_ME_COFFEE
+
+    constructor(username: String, email: String, avatar: Avatar)  {
+        this.username = username;
+        this.email = email
+        this.avatar = avatar
+    }
+
+    
+
+}
